5fdbb2537e86e659080e911646a4efffc9049fab
Tom Janssens <Tom@corebvba.be>
2011-03-07 23:20:34 +0100

    OMG removed session bootstrapper - duck tape dev FTW

---
+++ src/Org.NerdBeers/Org.NerdBeers.Web/Services/NerdBeersBootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Session;
+
+namespace Org.NerdBeers.Web.Services
+{
+    public class NerdBeersBootstrapper:DefaultNancyBootstrapper 
+    {
+        protected override void InitialiseInternal(TinyIoC.TinyIoCContainer container)
+        {
+            
+            base.InitialiseInternal(container);
+            CookieBasedSessions.Enable(this, "B0w4M|", "4IJAMREWT");
+
+        }
+    }
+}
